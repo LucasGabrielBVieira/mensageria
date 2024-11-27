c8f631ab3df2a1a9a2725f409324e51947e8e9f3
Lucas Gabriel Babolim Vieira <lucasvieira0420@gmail.com>
2024-11-27 14:39:03 -0300

    CodeBar Controller and RequestDTO

---
+++ api-boleto/src/main/java/com/lucasvieira/apiboleto/controller/BarCodeController.java
@@ -0,0 +1,26 @@
+package com.lucasvieira.apiboleto.controller;
+
+import com.lucasvieira.apiboleto.entity.dto.BarCodeDTO;
+import com.lucasvieira.apiboleto.entity.dto.BarCodeRequestDTO;
+import com.lucasvieira.apiboleto.service.BarCodeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/barcode")
+public class BarCodeController {
+
+    private final BarCodeService barCodeService;
+
+    public BarCodeController(BarCodeService barCodeService) {
+        this.barCodeService = barCodeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody BarCodeRequestDTO barCodeRequestDTO) {
+        BarCodeDTO barCodeDto = barCodeService.save(barCodeRequestDTO.getBarCode());
+        return new ResponseEntity<>(barCodeDto, HttpStatus.CREATED);
+    }
+
+}
